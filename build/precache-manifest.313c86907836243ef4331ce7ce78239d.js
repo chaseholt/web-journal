2c8ec23a797c7badd1572e6c7823bd0a365f65f7
Chase Holt <chaseholt@orc3.com>
2021-11-05 15:57:22 -0400

    changed public from public to build

---
+++ build/precache-manifest.313c86907836243ef4331ce7ce78239d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8652a0d2459c17c12ce504dbc11d56e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5fdffad7f8e7af74750",
+    "url": "/static/css/main.0e2b4aaa.chunk.css"
+  },
+  {
+    "revision": "8607764a93cfbef5ba23",
+    "url": "/static/js/2.299abf3e.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.299abf3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5fdffad7f8e7af74750",
+    "url": "/static/js/main.73258596.chunk.js"
+  },
+  {
+    "revision": "4f0fc17be28857f97a26",
+    "url": "/static/js/runtime-main.68fdd124.js"
+  }
+]);
